c8bfa801bde6b664e2fd70c084930b1e04f0e2ea
robert <keszegrobert@gmail.com>
2016-07-23 14:43:50 +0200

    added the protocol content page

--- client_app/platforms/browser/www/js/index.js
+++ client_app/platforms/browser/www/js/index.js
@@ -1,11 +1,59 @@
+var protocol_table = {};
+var current_protocol = -1;
+
+function loadJSON(jsonpath,callback) {   
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', jsonpath, true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+
 $(document).ready(function(){
-    jQuery.getJSON("protocols.json", function(data){    
-        elements = ''; 
-        for (counter in data.protocols)
+    console.log('document is ready')
+    loadJSON("protocols.json", function(data){
+        console.log('json file is loaded');
+        elements = '';
+        protocol_table = eval('('+data+')');
+        console.log('the version of the json file is: '+protocol_table.version);
+        console.log('the count of protocols is: '+protocol_table.protocols.length);
+        for (counter in protocol_table.protocols)
         {
-            p = data.protocols[counter];
-            elements += '<li><a href="#'+counter+'">'+p.name+'</a></li>';
+            var p = protocol_table.protocols[counter];
+            console.log(p);
+            elements += '<li>'
+                +'<a href="#pagetwo" data-transition="slide" '
+                +'rel="'+counter+'" class="protocol_item">'
+                +p.name
+                +'</a></li>';
         }
-        $('#lst_protocol').html(elements).listview("refresh");
+        $('#protocol_list').html(elements).listview("refresh");
+    });
+
+    $(document).on(
+        "click",
+        "a.protocol_item",
+        function(){ // When entering pagetwo
+            current_protocol = parseInt(jQuery(this).attr('rel'));
+        }
+    );
+
+    $(document).on("pagebeforeshow","#pagetwo",function(){ 
+        console.log('selected protocol:'+ current_protocol);
+        protocol = protocol_table.protocols[current_protocol];
+        elements = ''
+        for (index in protocol.sections)
+        {
+            var section = protocol.sections[index];
+            elements += '<h1>'+section.name+'</h1><p>'+section.content+'</p>';
+        }
+        $('#protocol_content').html(elements);
+        $('#protocol_title').html(protocol.name);
     });
 });
--- client_app/platforms/ios/www/js/index.js
+++ client_app/platforms/ios/www/js/index.js
@@ -1,11 +1,36 @@
+var protocol_table = {};
+var current_protocol = -1;
+
 $(document).ready(function(){
-    jQuery.getJSON("protocols.json", function(data){    
-        elements = ''; 
-        for (counter in data.protocols)
+    jQuery.getJSON("protocols.json", function(data){
+        elements = '';
+        protocol_table = data
+        for (counter in protocol_table)
         {
-            p = data.protocols[counter];
-            elements += '<li><a href="#'+counter+'">'+p.name+'</a></li>';
+            p = protocol_table[counter];
+            console.log(p);
+            elements += '<li>'
+                +'<a href="#pagetwo" data-transition="slide" '
+                +'rel="'+counter+'" class="protocol_item">'
+                +p.name
+                +'</a></li>';
         }
         $('#lst_protocol').html(elements).listview("refresh");
     });
+
+    $(document).on(
+        "click",
+        "a.protocol_item",
+        function(){ // When entering pagetwo
+            current_protocol = parseInt(jQuery(this).attr('rel'));
+        }
+    );
+
+    $(document).on("pagebeforeshow","#pagetwo",
+        function(){ // When entering pagetwo
+            console.log(current_protocol);
+
+    });
+
+
 });
